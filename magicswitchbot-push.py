74c829916ce1fc3641f7311067ab4375208c3a58
Torac <jyp.schuthof3@gmail.com>
2021-07-12 20:26:28 +0200

    Create magicswitchbot-push.py

---
+++ magicswitchbot-push.py
@@ -0,0 +1,32 @@
+from magicswitchbot import MagicSwitchbot
+import time, logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+MAC = "00:11:22:33:44:55"
+PASSWORD = None
+
+device = MagicSwitchbot(mac=MAC, connect_timeout=15, disconnect_timeout=10, password=PASSWORD)
+
+print(f"Connecting to MagicSwitchbot device at {MAC}...")
+
+res = device.get_battery()
+
+if res:
+
+    print(f"Connected to MagicSwitchbot device at {MAC} with {res}% of battery remaining")
+
+    time.sleep(1)
+
+    print("Turning on...")
+
+    ok = device.push()
+
+    if ok:
+        print("Command executed successfully")
+
+    else:
+        print("Error sending command")
+
+else:
+    print("Can not connect to device")
